de32e3811f0fdbc0841bb3b9ec79e28645acc5ca
Nicolas <nicopa47@gmail.com>
2026-03-23 20:14:31 -0300

    initial commit

---
+++ app.ts
@@ -0,0 +1,15 @@
+console.log('Prueba elvio')
+
+ type User = {
+    id: number;
+    name: string;
+    email: string;
+ } 
+
+const user: User = {
+    id: 1,
+    name: 'Paul Vaso',
+    email: 'paul.vaso.@ejemplo.com'
+}
+
+function getUser(id:number)
